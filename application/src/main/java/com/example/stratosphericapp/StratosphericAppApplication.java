c9d0543cd2f7b077e8654f9c8f50c37661b5f32d
Omer <taniyici.omer@gmail.com>
2024-03-03 05:07:42 -0500

    initial commit

---
+++ application/src/main/java/com/example/stratosphericapp/StratosphericAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stratosphericapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StratosphericAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StratosphericAppApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/com/example/stratosphericapp/StratosphericAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stratosphericapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StratosphericAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
